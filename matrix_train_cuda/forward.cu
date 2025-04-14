5b17719964ab14821fc6c00ea9c18b2ccdd641e5
saanna225 <sanha02468@gmail.com>
2025-04-13 21:53:34 -0500

    Updating forward.cu

--- matrix_train_cuda/forward.cu
+++ matrix_train_cuda/forward.cu
@@ -1,1 +1,15 @@
+#include <cuda_runtime.h>
 
+#define N 1024
+#define D 512
+
+_global_ void matmul_forward(float* x, float* w, float* y) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < N) {
+        for (int j = 0; j < D; ++j) {
+            y[idx * D + j] = 0;
+            for (int k = 0; k < D; ++k)
+                y[idx * D + j] += x[idx * D + k] * w[k * D + j];
+        }
+    }
+}
