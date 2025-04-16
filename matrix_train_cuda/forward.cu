c0a49c543642eaeedd2270fc05cde8554ef9f175
saanna225 <sanha02468@gmail.com>
2025-04-16 09:59:24 -0500

    Updating forward.cu
    missed headers and correcting multipliers

--- matrix_train_cuda/forward.cu
+++ matrix_train_cuda/forward.cu
@@ -1,4 +1,5 @@
 #include <cuda_runtime.h>
+#include <stdio.h>
 
 #define N 1024
 #define D 512
@@ -9,7 +10,7 @@ _global_ void matmul_forward(float* x, float* w, float* y) {
         for (int j = 0; j < D; ++j) {
             y[idx * D + j] = 0;
             for (int k = 0; k < D; ++k)
-                y[idx * D + j] += x[idx * D + k] * w[k * D + j];
+                y[idx * D + j] += x[idx * D + k] * w[k * D + j]; //dot product of idth row of x and jth col of w
         }
     }
 }
