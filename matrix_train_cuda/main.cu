5bb2a932e536262734592cdc41273234be3bc530
Sumukha M <sumukha266@gmail.com>
2025-04-23 00:02:31 -0500

    Initialize input tensors with sine and cosine values

--- matrix_train_cuda/main.cu
+++ matrix_train_cuda/main.cu
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <cuda_runtime.h>
 
-// Declare kernel functions from other files
 __global__ void matmul_forward(float* x, float* w, float* y);
 __global__ void matmul_backward(float* grad_out, float* x, float* grad_w);
 __global__ void sgd_update(float* w, float* grad_w, float lr);
@@ -14,10 +13,13 @@ int main() {
     float *x, *w, *y, *grad_out, *grad_w;
     float lr = 0.001f;
 
-    // Unified memory allocation
     cudaMallocManaged(&x, N * D * sizeof(float));
     cudaMallocManaged(&w, D * D * sizeof(float));
     cudaMallocManaged(&y, N * D * sizeof(float));
     cudaMallocManaged(&grad_out, N * D * sizeof(float));
     cudaMallocManaged(&grad_w, D * D * sizeof(float));
+
+    // Initialize x and w
+    for (int i = 0; i < N * D; ++i) x[i] = sin(i);
+    for (int i = 0; i < D * D; ++i) w[i] = cos(i);
 }
