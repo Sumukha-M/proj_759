25561fb29c50e8ad41e3e792591a9c00e2c6dbe1
Shraddha Singh <shraddha0897@gmail.com>
2025-04-24 13:30:29 -0500

    Minor optimization and formatting

--- matrix_train_cuda/backward.cu
+++ matrix_train_cuda/backward.cu
@@ -3,16 +3,13 @@
 #define N 1024
 #define D 512
 
-__global__ void matmul_backward(float* grad_out, float* x, float* grad_w) {
+_global_ void matmul_backward(float* grad_out, float* x, float* grad_w) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
-
     if (idx < D) {
         for (int j = 0; j < D; ++j) {
-            float sum = 0;
-            for (int b = 0; b < N; ++b) {
-                sum += x[b * D + idx] * grad_out[b * D + j];
-            }
-            grad_w[idx * D + j] = sum;
+            grad_w[idx * D + j] = 0;
+            for (int b = 0; b < N; ++b)
+                grad_w[idx * D + j] += x[b * D + idx] * grad_out[b * D + j];
         }
     }
 }
