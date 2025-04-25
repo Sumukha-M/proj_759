9f56349624917c6bcc3ef71e7d4fdbe7ba3f3589
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:13:09 -0500

    Add delay simulation comment

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -5,7 +5,7 @@
 
 void communicate_gradients() {
     for (int i = 0; i < 5; ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Simulate delay
         std::cout << "[Comm] AllReduce for Layer " << i << std::endl;
     }
 }
