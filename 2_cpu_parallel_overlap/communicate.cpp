24f783d7154b4390fa2726644ff7e081d6a0a967
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:07:19 -0500

    Rename markers to AllReduce

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,9 +3,9 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    std::cout << "Start Comm" << std::endl;
+    std::cout << "Start AllReduce" << std::endl;
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Syncing layer " << i << std::endl;
+        std::cout << "Layer: " << i << std::endl;
     }
-    std::cout << "End Comm" << std::endl;
+    std::cout << "Finish AllReduce" << std::endl;
 }
