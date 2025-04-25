12b62e6d48537f2879ddad33fde864d998f03907
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:27:46 -0500

    Add missing parallel before sections pragma

--- 2_cpu_parallel_overlap/main.cpp
+++ 2_cpu_parallel_overlap/main.cpp
@@ -1,14 +1,9 @@
-//==============================================
-// File: man.cpp
-// Desc: Entry OpenMP parallel execution
-//==============================================
-
 #include <omp.h>
 #include "compute.h"
 #include "communicate.h"
 
 int main() {
-#pragma omp sections
+#pragma omp parallel sections
     {
 #pragma omp section
         compute_gradients();
