1c39a0eb318be7239cac726264a6c9a5877a79d0
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:16:19 -0500

    fin.

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -11,7 +11,7 @@
 
 void communicate_gradients() {
     for (int i = 0; i < 5; ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Simulate communication delay
         std::cout << "[Comm] AllReduce for Layer " << i << std::endl;
     }
 }
