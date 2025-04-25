7f354af67f0f353c32caeaba6294dbe03ceed8f1
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:10:36 -0500

    Differentiate even and odd layers while communicating

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -4,18 +4,10 @@
 
 void communicate_gradients() {
     for (int i = 1; i <= 5; ++i) {
-        std::cout << "Layer #" << i << " is being processed." << std::endl;
+        if (i % 2 == 0) {
+            std::cout << "Even Layer: " << i << std::endl;
+        } else {
+            std::cout << "Odd Layer: " << i << std::endl;
+        }
     }
-}
-void communicate_weights() {
-    for (int i = 1; i <= 5; ++i) {
-        std::cout << "Layer #" << i << " is being processed." << std::endl;
-    }
-}
-void communicate() {
-    std::thread t1(communicate_gradients);
-    std::thread t2(communicate_weights);
-
-    t1.join();
-    t2.join();
 }
