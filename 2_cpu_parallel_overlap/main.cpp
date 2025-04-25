6221069bc3acf910b054cc03319c12b66e3546bd
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:28:40 -0500

    random function with pragma

--- 2_cpu_parallel_overlap/main.cpp
+++ 2_cpu_parallel_overlap/main.cpp
@@ -2,12 +2,17 @@
 #include "compute.h"
 #include "communicate.h"
 
+void random_function() {
+    #pragma omp section
+    {
+        compute_gradients();
+    }
+}
+
 int main() {
 #pragma omp parallel sections
     {
-#pragma omp section
-        compute_gradients();
-
+        random_function();
 #pragma omp section
         communicate_gradients();
     }
