352dce74c2b4ac60adea2ccf7c1da95904963b68
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:08:38 -0500

    Add sum calculation

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,8 +3,9 @@
 #include "communicate.h"
 
 void communicate_gradients() {
+    int sum = 0;
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Communicating layer " << i << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        sum += i;
     }
+    std::cout << "Sum of layers: " << sum << std::endl;
 }
