c7147f13eb02e1cdd2294543636f5de6a92c140a
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 00:02:35 -0500

    Initialize CUDA project with basic headers and constants

--- matrix_train_cuda/forward.cu
+++ matrix_train_cuda/forward.cu
@@ -3,15 +3,3 @@
 
 #define N 1024
 #define D 512
-
-__global__ void matmul_forward(float* x, float* w, float* y){
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < N) {
-        for (int j = 0; j < D; ++j) {
-            y[idx * D + j] = 0;
-            for (int k = 0; k < D; ++k)
-                y[idx * D + j] += x[idx * D + k] * w[k * D + j]; //dot product of idth row of x and jth col of w
-        }
-    }
-}
-
