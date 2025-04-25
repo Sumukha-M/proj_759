96cf56c3247e5a2e9bcc9a98fb2f20b1a1beb59c
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:04:31 -0500

    Fix function spelling but wrong loop range

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -2,8 +2,8 @@
 #include <thread>
 #include "communicate.h"
 
-void communiate_gradients() {
-    for (int i = 0; i < 5; i++) {
-        std::cout << "[Test] Layer " << i << std::endl;
+void communicate_gradients() {
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Communicating: Layer " << i << std::endl;
     }
 }
