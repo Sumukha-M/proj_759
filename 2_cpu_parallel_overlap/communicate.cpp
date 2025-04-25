4a02cb44ae5898ce2b5761e87f826e2a9608a8df
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:12:21 -0500

    Add sleep_for() of 70ms after each layer communication

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,10 +3,8 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    int fake_flag = 1;
-    if (fake_flag) {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "[Comm] Simulating AllReduce Layer " << i << std::endl;
-        }
+    for (int i = 0; i < 5; i++) {
+        std::cout << "[Comm] Transmitting Layer " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(70));
     }
 }
