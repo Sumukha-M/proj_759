6bfa3e29b77c2227ff4ab3d6b57fdd242238b0b8
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:05:07 -0500

    Add start and end markers

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,7 +3,9 @@
 #include "communicate.h"
 
 void communicate_gradients() {
+    std::cout << "Start Comm" << std::endl;
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Layer " << i << " - Done" << std::endl;
+        std::cout << "Syncing layer " << i << std::endl;
     }
+    std::cout << "End Comm" << std::endl;
 }
