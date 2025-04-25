7d2b5a9b8267b206d8751984ef5e63a9accda1a8
Sumukha M <sumukha266@gmail.com>
2025-04-25 16:31:15 -0500

    communicate header with basic function declaration (WIP)

---
+++ 2_cpu_parallel_overlap/communicate.h
@@ -0,0 +1,14 @@
+/*******************************************************
+ * File: communciate.h
+ * Desc: 
+ *  This file defines communcate_gradient function for AllReduce simulation.
+ * 
+ * Phase: 2 - Overlapping Projec with OpenMP
+ *******************************************************/
+
+#ifndef COMMUNCATE_H
+#define COMMUNCATE_H
+
+void communcate_gradient();
+
+#endif
