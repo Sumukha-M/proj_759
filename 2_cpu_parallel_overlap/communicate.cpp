5480717ff17070a88cedeed7ee967608f4631466
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:03:23 -0500

    Add iostream include and print AllReduce message

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,8 +1,9 @@
 #include <thread>
+#include <iostream>
 #include "communcate.h"
 
 void communcate_gradient() {
-    for (int i = 0; i <= 5; i++) {
-        std::cout << "Layer comm " << i << "\n";
+    for (int i = 0; i < 6; i++) {
+        std::cout << "AllReduce layer: " << i << "\n";
     }
 }
