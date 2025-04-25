3334469fb4ea740a3a81e29f4ac7d9806235e87a
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:12:45 -0500

    Standardize delay to 100ms across all layers

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -4,7 +4,7 @@
 
 void communicate_gradients() {
     for (int i = 0; i < 5; i++) {
-        std::cout << "[Comm] AllReduce completed for Layer " << i << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+        std::cout << "[Comm] AllReduce Layer " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
