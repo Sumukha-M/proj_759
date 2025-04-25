51a86c5f6bd701807b0c9c4a95843d3246bc0a0b
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:08:23 -0500

    Add sleep delay for each layer

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,9 +3,8 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    int dummy = 1;
     for (int i = 0; i < 5; ++i) {
-        dummy *= 2;
-        std::cout << "Dummy multiply: " << dummy << std::endl;
+        std::cout << "Communicating layer " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 }
