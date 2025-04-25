af8dd9c7ed91e983f2b86a0cd23d9097a7f24206
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 00:04:49 -0500

    Add basic structure for matmul kernel with thread indexing

--- matrix_train_cuda/forward.cu
+++ matrix_train_cuda/forward.cu
@@ -3,3 +3,12 @@
 
 #define N 1024
 #define D 512
+
+__global__ void matmul_forward(float* x, float* w, float* y) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // Work only if thread index is within matrix row bounds
+    if (idx < N) {
+        // Future matrix computation will go here
+    }
+}
