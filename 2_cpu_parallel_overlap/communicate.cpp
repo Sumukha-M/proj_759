2c309f575e9e32c1a17af25d930a13f4ec1bd457
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:07:29 -0500

    Test nested loops for communication

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,9 +3,10 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    std::cout << "Start AllReduce" << std::endl;
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Layer: " << i << std::endl;
+    for (int j = 0; j < 2; ++j) {
+        std::cout << "Outer loop " << j << std::endl;
+        for (int i = 0; i < 5; ++i) {
+            std::cout << "Layer: " << i << std::endl;
+        }
     }
-    std::cout << "Finish AllReduce" << std::endl;
 }
