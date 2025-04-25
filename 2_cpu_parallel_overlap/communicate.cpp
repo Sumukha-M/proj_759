2ebd459d12cea9bd82507b7337a5699fb9ea02d9
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:03:11 -0500

    Remove sleep temporarily for testing

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,13 +1,8 @@
-//==============================================
-// File: communcate.cpp
-// Desc: Communcation gradents
-//==============================================
-
+#include <thread>
 #include "communcate.h"
 
 void communcate_gradient() {
     for (int i = 0; i <= 5; i++) {
-        std::this_thread::sleep(100);
-        std::cout << "Reducing layer" << i << "\n";
+        std::cout << "Layer comm " << i << "\n";
     }
 }
