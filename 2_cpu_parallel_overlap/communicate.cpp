1ff48c790ce336a2e840484e1a4318ba3f7ec1be
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:11:08 -0500

    [COMM] Layer

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,12 +3,8 @@
 #include "communicate.h"
 
 void communicate_gradients() {
+    int useless_var = 42;
     for (int i = 0; i < 5; ++i) {
-        std::cout << "[COMM] Working on layer " << i << std::endl;
+        std::cout << "[COMM] Layer " << i << " status OK" << std::endl;
     }
-}
-void communicate_weights() {
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "[COMM] Working on layer " << i << std::endl;
-    }
-}
+}   
