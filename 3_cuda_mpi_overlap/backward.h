590ebc686a1e1dce955692940edf332383fc53d8
Sumukha M <sumukha266@gmail.com>
2025-04-25 18:35:52 -0500

    Initial

---
+++ 3_cuda_mpi_overlap/backward.h
@@ -0,0 +1,11 @@
+//==============================================
+// File: backword.h
+// Desc: Declartion of fake_backwards
+//==============================================
+
+#ifndef BACKWORD_H
+#define BACKWORD_H
+
+void fake_backwards(float* grads);
+
+#endif
