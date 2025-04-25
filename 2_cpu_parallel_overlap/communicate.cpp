7458f99adec5237fdb6c3a735a998831a5a28d66
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:07:39 -0500

    Add extra print after each layer

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,10 +3,8 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    for (int j = 0; j < 2; ++j) {
-        std::cout << "Outer loop " << j << std::endl;
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "Layer: " << i << std::endl;
-        }
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Layer communication - " << i << std::endl;
+        std::cout << "Wait..." << std::endl;
     }
 }
