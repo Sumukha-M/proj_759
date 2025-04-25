004621683e5f831af403cf61099937b338ce216a
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:12:57 -0500

    Fix missing chrono include

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 #include "communicate.h"
 
 void communicate_gradients() {
-    for (int i = 0; i < 5; i++) {
-        std::cout << "[Comm] AllReduce Layer " << i << std::endl;
+    for (int i = 0; i < 5; ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "[Comm] AllReduce for Layer " << i << std::endl;
     }
 }
