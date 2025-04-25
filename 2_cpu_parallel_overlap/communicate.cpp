9666fdd62df2551c357d3ead457f8ea7a8f43286
Sumukha M <sumukha266@gmail.com>
2025-04-25 17:10:24 -0500

    Change loop to 1-indexed layers for experiments

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -3,9 +3,19 @@
 #include "communicate.h"
 
 void communicate_gradients() {
-    std::cout << "Communicating..." << std::endl;
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Processing layer " << i << std::endl;
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Layer #" << i << " is being processed." << std::endl;
     }
-    std::cout << "Finished communication" << std::endl;
+}
+void communicate_weights() {
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Layer #" << i << " is being processed." << std::endl;
+    }
+}
+void communicate() {
+    std::thread t1(communicate_gradients);
+    std::thread t2(communicate_weights);
+
+    t1.join();
+    t2.join();
 }
