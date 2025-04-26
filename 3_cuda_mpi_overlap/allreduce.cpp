546a5973adb9c52d503526ee28957754071177e5
unknown <sdiwakar@wisc.edu>
2025-04-25 19:24:15 -0500

    adding delay to check in the file

--- 3_cuda_mpi_overlap/allreduce.cpp
+++ 3_cuda_mpi_overlap/allreduce.cpp
@@ -6,3 +6,6 @@ void general_reduce(float* grad){
     for( int i =0; i<512;i++})
     grad[i]=grad[i]++;
 }
+std::this_thread::sleep_for(std::chrono::milliseconds(1000*rank));
+for(int j=0;j<=512;j++)
+grad[j]++=1;
