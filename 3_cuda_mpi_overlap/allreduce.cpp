c262321de73d9c06180dfeaf9a91866bc9b2f272
unknown <sdiwakar@wisc.edu>
2025-04-25 19:46:23 -0500

    making loop function work correctly

--- 3_cuda_mpi_overlap/allreduce.cpp
+++ 3_cuda_mpi_overlap/allreduce.cpp
@@ -4,12 +4,10 @@
 const int D=512;
 // simulates allreduce by applying delay and scaling gradients//
 void fake_reduce(float* grad,int rank,int size){ 
-    for( int i =0; i<512;++i)
     grad[i]*=size;//num of participating processes//
 }
-// #pragma omp parallel for(int i=0;i<512;+=i)
-// grad[i]* =size;
-std::this_thread::sleep_for(std::chrono::milliseconds(150));
-for(int j=0;j<512;++j)
-grad[i]*=(rank+1);
+
+std::this_thread::sleep_for(std::chrono::millisecond(100));
+for(int i=0;i<512;++i)
+grad[i]*=size;
 std::cout<<"getting allreduce output";
