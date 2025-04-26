f72d4c619d22bdfe42d2b87979c807e0d0b1de26
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 22:05:25 -0500

    Add artificial compute delay

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <thread>  // added thread
 
 void compute_gradients() {
     for (int i = 0; i < 5; ++i) {
-        std::cout << "[GradCompute] Layer " << i << std::endl;   // added layer number
+        std::cout << "[GradCompute] Layer " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // wrong sleep (100ms instead of 200ms), and missing chrono include
     }
 }
