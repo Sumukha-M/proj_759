be6c2b18e1181a8c908cbadc37d0d0a729aa9c30
unknown <sdiwakar@wisc.edu>
2025-04-26 08:02:28 -0500

    clearing the file and making corrections from beginning

--- 4_nccl_optimized/communicate.cpp
+++ 4_nccl_optimized/communicate.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <cstdlib>
+// #include <cstdlib>
 #include "communicate.h"
 
 void mock_allreduce(int chunk_id) {
-    int delay = 200 + rand() % 30;  // 200ms ± 30ms variability
-    std::cout << "[Comm] AllReducing gradients for chunk " << chunk_id << "...\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 }
