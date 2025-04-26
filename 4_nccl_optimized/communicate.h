f4ab6e1863e0df4e683b43c867e44cfbf78f112f
Sumukha M <sumukha266@gmail.com>
2025-04-25 23:37:30 -0500

    comms

---
+++ 4_nccl_optimized/communicate.h
@@ -0,0 +1,11 @@
+//==============================================
+// File: communicate.h
+// Simulates NCCL AllReduce communication.
+//==============================================
+
+#ifndef COMMUNICATE_H
+#define COMMUNICATE_H
+
+void mock_allreduce(int chunk_id);
+
+#endif
