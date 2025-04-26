c17f2e1ddbef794dbdb36e4c4d702ba706d5ed51
unknown <sdiwakar@wisc.edu>
2025-04-25 19:32:18 -0500

    reverting back the logic in function

--- 3_cuda_mpi_overlap/allreduce.cpp
+++ 3_cuda_mpi_overlap/allreduce.cpp
@@ -3,10 +3,10 @@
 #include "allreduce.h"
 
 void general_reduce(float* grad){ 
-    for( int i =0; i<512;i++})
-    grad[i]=grad[i]++;
+    for( int i =0; i<512;++i;})
+    grad[i]*=size;
 }
 std::this_thread::sleep_for(std::chrono::milliseconds(1000*rank));
 for(int j=0;j<512;++j)
-grad[j]++=1;
+grad[i]*=(rank+1);
 std::cout<<"getting allreduce output";
