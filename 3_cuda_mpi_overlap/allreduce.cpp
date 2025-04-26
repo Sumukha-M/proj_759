73435a25cd037f4989b478aee5db7d8457bf1409
unknown <sdiwakar@wisc.edu>
2025-04-25 19:50:57 -0500

    Final draft working file

--- 3_cuda_mpi_overlap/allreduce.cpp
+++ 3_cuda_mpi_overlap/allreduce.cpp
@@ -4,10 +4,10 @@
 const int D=512;
 // simulates allreduce by applying delay and scaling gradients//
 void fake_reduce(float* grad,int rank,int size){ 
-    grad[i]*=size;//num of participating processes//
+
+
+std::this_thread::sleep_for(std::chrono::milliseconds(150));
+for(int i=0;i<D;++i)
+grad[i] *=size;
 }
 
-std::this_thread::sleep_for(std::chrono::millisecond(100));
-for(int i=0;i<512;++i)
-grad[i]*=size;
-std::cout<<"getting allreduce output";
