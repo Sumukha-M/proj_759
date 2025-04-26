543d801eecb117a3a30897d961ebc3f7bdb57b83
unknown <sdiwakar@wisc.edu>
2025-04-25 19:43:12 -0500

    correcting typos and recognising faults

--- 3_cuda_mpi_overlap/allreduce.cpp
+++ 3_cuda_mpi_overlap/allreduce.cpp
@@ -1,13 +1,14 @@
 #include <thread>
 #include <chrono>
 #include "allreduce.h"
+const int D=512;
 // simulates allreduce by applying delay and scaling gradients//
-void general_reduce(float* grad,int rank,int size){ 
+void fake_reduce(float* grad,int rank,int size){ 
     for( int i =0; i<512;++i)
-    grad[i]*=size;
+    grad[i]*=size;//num of participating processes//
 }
-#pragma omp parallel for(int i=0;i<512;+=i)
-grad[i]* =size;
+// #pragma omp parallel for(int i=0;i<512;+=i)
+// grad[i]* =size;
 std::this_thread::sleep_for(std::chrono::milliseconds(150));
 for(int j=0;j<512;++j)
 grad[i]*=(rank+1);
