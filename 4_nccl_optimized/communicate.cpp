28c7f83a9cfba977d40f4d7908aa815bd0d6e900
Sumukha M <sumukha266@gmail.com>
2025-04-26 00:07:15 -0500

    Updated communicate.cpp with Random Delay

--- 4_nccl_optimized/communicate.cpp
+++ 4_nccl_optimized/communicate.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <cstdlib>
 #include "communicate.h"
 
-// Simulate NCCL AllReduce for a given chunk
 void mock_allreduce(int chunk_id) {
+    int delay = 200 + rand() % 30;  // 200ms ± 30ms variability
     std::cout << "[Comm] AllReducing gradients for chunk " << chunk_id << "...\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));  // Simulate communication time
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 }
