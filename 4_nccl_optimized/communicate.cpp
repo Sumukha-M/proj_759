ab38dcf685876848f64dabfcfae4084bfd67565d
unknown <sdiwakar@wisc.edu>
2025-04-26 08:11:28 -0500

    fixing bugs

--- 4_nccl_optimized/communicate.cpp
+++ 4_nccl_optimized/communicate.cpp
@@ -6,7 +6,7 @@
 #include "communicate.h"
 
 void all_reduce(int chunk_id) {
-    std::cout<<"starting all reduce \n";
-    std::this_thread::sleep_for(std::chrono::millisecond(10000));
-    int delay =200+rand()%20;
+    std::cout<<"all reducing gradients for chunk"<<chunk_id;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    int delay =200+rand()%20; // using chunk_id for delay purposes
 }
