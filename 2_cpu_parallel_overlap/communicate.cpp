b15d5c9cc282d1365405f87be418b3221c120dc7
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 22:05:58 -0500

    Fix missing chrono include

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <thread>  // added thread
+#include <thread>
+#include <chrono>  // added chrono
 
 void compute_gradients() {
     for (int i = 0; i < 5; ++i) {
         std::cout << "[GradCompute] Layer " << i << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // wrong sleep (100ms instead of 200ms), and missing chrono include
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
