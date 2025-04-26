dd1b1c2c4f8c6feaa31c0b009e2d223d87bcf684
Sumukha M <sumukha266@gmail.com>
2025-04-25 23:37:11 -0500

    comp[ute

---
+++ 4_nccl_optimized/compute.h
@@ -0,0 +1,11 @@
+//==============================================
+// File: compute.h
+// Simulates gradient computation in chunks.
+//==============================================
+
+#ifndef COMPUTE_H
+#define COMPUTE_H
+
+void compute_gradients(int chunk_id);
+
+#endif
