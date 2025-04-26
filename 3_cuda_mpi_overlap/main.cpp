a3e979346b4e52d70786db8a3e1f6685b49b170d
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:44:11 -0500

    Add simulated delay before AllReduce to better overlap compute and communication

--- 3_cuda_mpi_overlap/main.cpp
+++ 3_cuda_mpi_overlap/main.cpp
@@ -21,12 +21,13 @@ int main(int argc, char** argv) {
 #pragma omp section
         {
             fake_backward(grad);
-            std::cout << "[Rank " << rank << "] Backward pass done." << std::endl;
+            std::cout << "[Rank " << rank << "] Gradient computed" << std::endl;
         }
 #pragma omp section
         {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
             fake_allreduce(grad, rank, size);
-            std::cout << "[Rank " << rank << "] AllReduce done." << std::endl;
+            std::cout << "[Rank " << rank << "] Gradient AllReduced" << std::endl;
         }
     }
 
