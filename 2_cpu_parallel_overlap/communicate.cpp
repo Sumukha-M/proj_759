536cb292cc50c9f47cbc1f63e0edf7c209021322
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 22:03:42 -0500

    Fix function name to compute_gradients

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-void comp_grad() {
+void compute_gradients() {   // fix function name
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Computing Layer\n";   
+        std::cout << "Computing Layer\n"; 
     }
 }
