bfe365cffc898e47bf3af57fe30ade6d87b78b78
Sumukha M <sumukha266@gmail.com>
2025-04-25 19:20:34 -0500

    start

---
+++ 3_cuda_mpi_overlap/allreduce.h
@@ -0,0 +1,11 @@
+//==============================================
+// File: allreduc.h
+// Desc: Declration of fake_allreduce
+//==============================================
+
+#ifndef ALLREDUC_H
+#define ALLREDUC_H
+
+void fake_allreduces(float* gradients, int rnk, int sz);
+
+#endif
