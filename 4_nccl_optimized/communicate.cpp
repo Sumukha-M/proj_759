450a1f53745938ccae8b0186eeb08bf5dd191f71
unknown <sdiwakar@wisc.edu>
2025-04-26 08:08:38 -0500

    checking the delay working

--- 4_nccl_optimized/communicate.cpp
+++ 4_nccl_optimized/communicate.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-// #include <cstdlib>
+#include <cstdlib>
 #include <time.h>
 #include "communicate.h"
 
-void mock_allreduce(int chunk_id) {
+void all_reduce(int chunk_id) {
     std::cout<<"starting all reduce \n";
     std::this_thread::sleep_for(std::chrono::millisecond(10000));
+    int delay =200+rand()%20;
 }
