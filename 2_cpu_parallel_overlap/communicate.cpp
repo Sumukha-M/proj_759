58094f0b52e7e06f39359a3fbb39d6374d520aa7
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 22:03:00 -0500

    Add comp_grad function

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,17 +1,7 @@
-//==============================================
-// File: communicate.cpp
-// Description: Implements communicate_gradients function.
-// Simulates communication (AllReduce) with delays.
-//==============================================
-
 #include <iostream>
-#include <thread>
-#include <chrono>
-#include "communicate.h"
 
-void communicate_gradients() {
+void comp_grad() {
     for (int i = 0; i < 5; ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Simulate communication delay
-        std::cout << "[Comm] AllReduce for Layer " << i << std::endl;
+        std::cout << "Computing Layer\n";   
     }
 }
