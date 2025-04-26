86a11cd944c30ad957fcca33764805e4f1217d01
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:21:47 -0500

    back to grad for consistency

--- 3_cuda_mpi_overlap/backward.cpp
+++ 3_cuda_mpi_overlap/backward.cpp
@@ -3,7 +3,7 @@
 const int D = 512;
 
 // Simulate backward pass (gradient computation)
-void fake_backward(float* gradients) {
+void fake_backward(float* grad) {
     for (int i = 0; i < D; ++i)
-        gradients[i] = i;
+        grad[i] = i;
 }
