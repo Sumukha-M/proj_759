2a25654113adc2912aa94fe38437fe843a9b8dc0
Shraddha Singh <shraddha0897@gmail.com>
2025-04-25 22:06:40 -0500

    Fix compute delay to 200ms

--- 2_cpu_parallel_overlap/communicate.cpp
+++ 2_cpu_parallel_overlap/communicate.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <thread>
-#include <chrono>  // added chrono
+#include <chrono>
 
 void compute_gradients() {
     for (int i = 0; i < 5; ++i) {
         std::cout << "[GradCompute] Layer " << i << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));  // correct sleep to 200ms
     }
 }
