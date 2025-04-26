d866723e9437301d0810ed3d076cdb45e7db866c
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:21:35 -0500

    gradient computation

--- 3_cuda_mpi_overlap/backward.cpp
+++ 3_cuda_mpi_overlap/backward.cpp
@@ -2,7 +2,7 @@
 
 const int D = 512;
 
-// Simulate backward pass
+// Simulate backward pass (gradient computation)
 void fake_backward(float* gradients) {
     for (int i = 0; i < D; ++i)
         gradients[i] = i;
