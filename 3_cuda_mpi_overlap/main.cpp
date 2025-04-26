32156aa4c07a2bfa1f9b1246d11d6b8a32fa9f53
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:43:44 -0500

    constant D for gradient array size

--- 3_cuda_mpi_overlap/main.cpp
+++ 3_cuda_mpi_overlap/main.cpp
@@ -5,21 +5,23 @@
 #include "backward.h"
 #include "allreduce.h"
 
+const int D = 512;
+
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
-    float gradient[512];
+    float grad[D];
 
 #pragma omp parallel sections
     {
 #pragma omp section
         {
-            fake_backward(gradient);
+            fake_backward(grad);
             std::cout << "Backward completed" << std::endl;
         }
 #pragma omp section
         {
-            fake_allreduce(gradient, 0, 1);
+            fake_allreduce(grad, 0, 1);
             std::cout << "AllReduce completed" << std::endl;
         }
     }
