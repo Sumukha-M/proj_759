06f3b6577d77c6dbf9b3c744134781a3c0dae82a
unknown <sdiwakar@wisc.edu>
2025-04-26 08:05:51 -0500

    adding the function flow

--- 4_nccl_optimized/communicate.cpp
+++ 4_nccl_optimized/communicate.cpp
@@ -2,7 +2,10 @@
 #include <thread>
 #include <chrono>
 // #include <cstdlib>
+#include <time.h>
 #include "communicate.h"
 
 void mock_allreduce(int chunk_id) {
+    std::cout<<"starting all reduce \n";
+    std::this_thread::sleep_for(std::chrono::millisecond(10000));
 }
