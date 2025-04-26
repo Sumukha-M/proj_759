513d053bd1c1e168dd69c901fc7a517f9e6a8081
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:20:44 -0500

    function

--- 3_cuda_mpi_overlap/backward.cpp
+++ 3_cuda_mpi_overlap/backward.cpp
@@ -3,6 +3,6 @@
 const int d = 512;
 
 // Simulate backward pass
-void fake_backwards(float* gradients) {
+void fake_backward(float* gradients) {
     for (int i = 0; i <= d; i++) gradients[i] = i;
 }
