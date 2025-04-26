18317576afa3483b9d4d7335936c5bfb0f50cef4
Sumukha M <sumukha266@gmail.com>
2025-04-25 22:50:30 -0500

    Polish output messages to reflect successful stages

--- 3_cuda_mpi_overlap/main.cpp
+++ 3_cuda_mpi_overlap/main.cpp
@@ -16,20 +16,20 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    float grad[D] = {0};
+    float grad[D];
 
 #pragma omp parallel sections
     {
 #pragma omp section
         {
             fake_backward(grad);
-            std::cout << "[Rank " << rank << "] Computation complete." << std::endl;
+            std::cout << "[Rank " << rank << "] Gradient computed successfully." << std::endl;
         }
 #pragma omp section
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             fake_allreduce(grad, rank, size);
-            std::cout << "[Rank " << rank << "] Communication complete." << std::endl;
+            std::cout << "[Rank " << rank << "] AllReduce finished successfully." << std::endl;
         }
     }
 
